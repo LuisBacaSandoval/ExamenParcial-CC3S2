29502f8f1bfd0c84af4c02aa72b58872e290fc6b
LuisBacaSandoval <luis.baca.s@uni.pe>
2022-11-10 09:24:49 -0500

    finish question 1.a

--- Pregunta1/src/Panda.java
+++ Pregunta1/src/Panda.java
@@ -1,15 +1,33 @@
+//Se importa el paquete java.util.function.* para poder usar
+//las expresiones lambda
 import java.util.function.*;
 
+//Clase Panda
 public class Panda {
-int age;
- public static void main(String[] args) {
-         Panda p1 = new Panda();
-         p1.age = 1;
-         check(p1, p -> p.age < 5);
-         }
-private static void check(Panda panda,
- Predicate<Panda> pred) {
-         String result =
-                 pred.test(panda)?"match":"not match";
-         System.out.print(result);
-         } }
+    //Atributos
+    int age;
+
+    //Metodo main()
+    public static void main(String[] args) {
+        //Se instancia el objeto p1 del tipo Panda
+        Panda p1 = new Panda();
+        //Se le asigna el valor de 1 al atributo age
+        p1.age = 1;
+        //Se llama al metodo check()
+        check(p1, p -> p.age < 5);
+    }
+
+    //Metodo check() recibe por atributos un objeto Panda y una expresion Lambda,
+    //esta expresion Lambda(Predicate) lo que hace es comprobar si la condicion p.age < 5
+    //es true or false
+    private static void check(Panda panda,
+                              Predicate<Panda> pred) {
+        //Se le asigna a la variable result el valor "match" en el caso la expresion lambda sea true
+        //si no se le asigna el valor "not match"
+        String result =
+                pred.test(panda) ? "match" : "not match";
+        //Finalmente imprimos el resultado, en este caso lo que se mostrara en consola es: "match"
+        // debido a que la expresion lambda(p.age < 5) nos da true
+        System.out.print(result);
+    }
+}
